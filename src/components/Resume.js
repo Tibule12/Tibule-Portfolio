43e8844adec3ed243d1d302f19c3e843dbd6e293
Tibule12 <thulani.mtshwelo@capaciti.org.za>
2025-06-06 11:44:23 +0200

    Fix resume download link to use PUBLIC_URL and download attribute for direct download

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <section id="resume" className="section">
       <h2>Download My Resume</h2>
-      <a href="/images/Thulani_Mtshwelo_CV_Updated.pdf" download className="cta-button">Download Resume</a>
+      <a href={process.env.PUBLIC_URL + "/images/Thulani_Mtshwelo_CV_Updated.pdf"} download className="cta-button">Download Resume</a>
     </section>
   );
 };
